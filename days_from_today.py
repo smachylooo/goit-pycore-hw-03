797967ac5891c779035ecff33c3b60839e4be062
Bohdna Smachylo <bohdan@windowmar.ca>
2026-02-05 16:46:28 -0700

    ADD: Days from today Function

---
+++ days_from_today.py
@@ -0,0 +1,12 @@
+import datetime as dt
+
+def get_days_from_today(date: str) -> int:
+    try:
+        date = dt.datetime.strptime(date, '%Y-%m-%d').date()
+    except (ValueError, TypeError) :
+        return 'Not Valid formt! Should be YYYY-MM-DD as string'
+
+    today = dt.date.today()
+    days_from_date = (today - date).days
+    
+    return int(days_from_date)
