f443497ef2bc036f41e0eda3666956c5a64303f8
Bohdna Smachylo <bohdan@windowmar.ca>
2026-02-11 10:46:50 -0700

    ADD: Birthady notification FUNC

--- birthday_notification.py
+++ birthday_notification.py
@@ -1,2 +1,18 @@
-def get_upcoming_birthdays():
-    pass
+import datetime as dt
+def get_upcoming_birthdays(users: list[dict]) -> list[dict]:
+    res = []
+    today = dt.datetime.today().date()
+    for user in users:
+        birthday_this_year = dt.datetime.strptime(user['birthday'], '%Y.%m.%d').date().replace(year=today.year)
+        days_diff = (birthday_this_year - today).days
+
+    if 0 < days_diff <= 9:
+        shift_days = {5: 2, 6: 1}.get(birthday_this_year.weekday(), 0)
+        birthday_this_year += dt.timedelta(days=shift_days)
+
+        res.append({
+            'name': user['name'],
+            'congratulation_date': birthday_this_year.strftime("%Y.%m.%d"),
+        })
+
+    return res
